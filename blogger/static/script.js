09f998f9230bcaa4b2258535a35e890448b1f79a
Linda <lindathehuang@gmail.com>
2020-09-28 18:43:31 -0700

    Added Confetti

--- blogger/static/script.js
+++ blogger/static/script.js
@@ -5,8 +5,8 @@ $( document ).ready(function() {
             "opacity":"0.75"
         })
     }
-});
 
-// document.getElementById('herobanner').onerror = function() {
-//     document.getElementById('herobanner').setAttribute('src', 'https://images.unsplash.com/photo-1473773508845-188df298d2d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2534&q=80');
-// }
+    $("#confetti").click(function(){
+        confetti.start(1000, 100);
+    })
+});
